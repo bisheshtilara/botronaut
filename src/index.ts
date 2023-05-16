0d46a53e269cf8ef6f0444d2cf3909056f23ecb8
Bishesh Tilara <bisheshtilara@gmail.com>
2023-05-16 14:43:40 +0200

    feat: added top 5 reddit posts from a specific subreddit with axios

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,28 @@
 import "dotenv/config"
-import * as Eris from "eris"
+import Eris from "eris"
+import axios from "axios"
 
 const bot = new Eris.Client(process.env.TOKEN ? process.env.TOKEN : "")
+const api = "https://www.reddit.com/r/diablo4/hot.json"
 
 bot.on("ready", () => {
   console.log("Ready!", bot.user.username + "#" + bot.user.discriminator)
 })
 
 bot.on("messageCreate", (msg) => {
-  console.log("content", msg.content)
-  if (msg.content.toLowerCase() === "!ping") bot.createMessage(msg.channel.id, "pong!")
+  if (msg.content.toLowerCase() === "!reddit") {
+    axios
+      .get(api)
+      .then((res) => {
+        res.data.data.children
+          .filter((post: any) => post.data.ups > 100)
+          .sort((a: any, b: any) => b.data.ups - a.data.ups)
+          .slice(0, 5)
+          .map((post: any) => post.data.url)
+          .forEach((url: any) => bot.createMessage(msg.channel.id, url))
+      })
+      .catch((e) => console.log(e))
+  }
 })
 
 bot.connect()
