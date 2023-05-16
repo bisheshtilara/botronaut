20aec250c2b55e98165de7a3b5b22d2f5e2a9270
Bishesh Tilara <bisheshtilara@gmail.com>
2023-05-16 12:43:51 +0200

    feat: added eris and initialized bot pong back on ping message| removed discord.js

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
-import 'dotenv/config'
-console.log('Hello Botronaut!')
-console.log(process.env.BOT_TOKEN)
+import "dotenv/config"
+import * as Eris from "eris"
+
+const bot = new Eris.Client(process.env.TOKEN ? process.env.TOKEN : "")
+
+bot.on("ready", () => {
+  console.log("Ready!", bot.user.username + "#" + bot.user.discriminator)
+})
+
+bot.on("messageCreate", (msg) => {
+  console.log("content", msg.content)
+  if (msg.content.toLowerCase() === "!ping") bot.createMessage(msg.channel.id, "pong!")
+})
+
+bot.connect()
