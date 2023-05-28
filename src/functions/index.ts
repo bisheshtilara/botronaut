986fa3a626f578a65161088d853cd43546ca4c95
Bishesh Tilara <bisheshtilara@gmail.com>
2023-05-28 03:01:11 +0200

    feat: structred functions

---
+++ src/functions/index.ts
@@ -0,0 +1,59 @@
+import axios from "axios"
+import { Message } from "discord.js"
+
+const getSubredditIcon = async (subreddit: string) => {
+  const icon = await axios
+    .get(`https://www.reddit.com/r/${subreddit}/about.json`)
+    .then((res) => res.data.data.icon_img)
+    .catch((e) => console.log(e))
+  return icon
+}
+
+export const getRedditPosts = (api: string, message: Message) => {
+  return axios
+    .get(api)
+    .then((res) => {
+      res.data.data.children
+        .filter((post: any) => post.data.ups > 500)
+        .sort((a: any, b: any) => b.data.ups - a.data.ups)
+        .slice(0, 5)
+        .map((post: any) => ({
+          title: post.data.title,
+          post_hint: post.data.post_hint,
+          author: post.data.author,
+          ups: post.data.ups,
+          downs: post.data.downs,
+          permalink: post.data.permalink,
+          url: post.data.url,
+          subreddit_name_prefixed: post.data.subreddit_name_prefixed,
+          subreddit: post.data.subreddit,
+        }))
+        .forEach(async ({ title, post_hint, author, permalink, url, ups, subreddit_name_prefixed, subreddit }: any) => {
+          const icon = await getSubredditIcon(subreddit)
+          message.channel.send({
+            embeds: [
+              {
+                title,
+                color: 0xff0000,
+                author: {
+                  name: subreddit_name_prefixed,
+                  icon_url: icon,
+                  url: `https://www.reddit.com/r/diablo4/`,
+                },
+                fields: [
+                  { name: "Author", value: `u/${author}`, inline: true },
+                  { name: "Upvotes", value: ups, inline: true },
+                ],
+                footer: {
+                  text: permalink,
+                  icon_url: `https://i.imgur.com/tyOb1Uk.jpeg`,
+                },
+                url: `https://www.reddit.com${permalink}`,
+                image: { url: post_hint === "image" ? url : `https://i.imgur.com/tyOb1Uk.jpeg` },
+              },
+            ],
+          })
+        })
+    })
+    .catch((e) => console.log(e))
+}
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,9 @@
 import axios from "axios"
 import "dotenv/config"
 import { Client, Events, GatewayIntentBits, Message } from "discord.js"
+import { getRedditPosts } from "./functions"
 
 const api = "https://www.reddit.com/r/diablo4/hot.json"
-
-const getSubredditIcon = async (subreddit: string) => {
-  const icon = await axios
-    .get(`https://www.reddit.com/r/${subreddit}/about.json`)
-    .then((res) => res.data.data.icon_img)
-    .catch((e) => console.log(e))
-  return icon
-}
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages],
@@ -25,67 +18,18 @@ client.on(Events.MessageCreate, (message) => {
 })
 
 client.on(Events.MessageCreate, (message) => {
-  if (message.content.toLowerCase() === "!reddit")
-    axios
-      .get(api)
-      .then((res) => {
-        res.data.data.children
-          .filter((post: any) => post.data.ups > 500)
-          .sort((a: any, b: any) => b.data.ups - a.data.ups)
-          .slice(0, 1)
-          .map((post: any) => ({
-            title: post.data.title,
-            post_hint: post.data.post_hint,
-            author: post.data.author,
-            ups: post.data.ups,
-            downs: post.data.downs,
-            permalink: post.data.permalink,
-            url: post.data.url,
-            subreddit_name_prefixed: post.data.subreddit_name_prefixed,
-            subreddit: post.data.subreddit,
-          }))
-          .forEach(
-            async ({ title, post_hint, author, permalink, url, ups, subreddit_name_prefixed, subreddit }: any) => {
-              const icon = await getSubredditIcon(subreddit)
-              message.channel.send({
-                embeds: [
-                  {
-                    title,
-                    color: 0xff0000,
-                    author: {
-                      name: subreddit_name_prefixed,
-                      icon_url: icon,
-                      url: `https://www.reddit.com/r/diablo4/`,
-                    },
-                    fields: [
-                      { name: "Author", value: `u/${author}`, inline: true },
-                      { name: "Upvotes", value: ups, inline: true },
-                    ],
-                    footer: {
-                      text: permalink,
-                      icon_url: `https://i.imgur.com/tyOb1Uk.jpeg`,
-                    },
-                    url: `https://www.reddit.com${permalink}`,
-                    image: { url: post_hint === "image" ? url : `https://i.imgur.com/tyOb1Uk.jpeg` },
-                  },
-                ],
-              })
-            }
-          )
-      })
-      .catch((e) => console.log(e))
+  if (message.content.toLowerCase() === "!reddit") getRedditPosts(api, message)
 })
 
-async function clearMessages(channel: any) {
-  const fetched = await channel.messages.fetch({ limit: 100 })
-  if (fetched.size > 0) {
-    await channel.bulkDelete(fetched)
-    clearMessages(channel)
+client.on(Events.MessageCreate, async (message) => {
+  if (message.content.toLowerCase() === "!clear") {
+    if (message.channel.type === 0) {
+      let fetched
+      while ((fetched = await message.channel.messages.fetch({ limit: 100 })).size > 0) {
+        await message.channel.bulkDelete(fetched)
+      }
+    }
   }
-}
-
-client.on(Events.MessageCreate, (message) => {
-  if (message.content.toLowerCase() === "!clear") clearMessages(message.channel)
 })
 
 client.login(process.env.TOKEN ? process.env.TOKEN : "")
